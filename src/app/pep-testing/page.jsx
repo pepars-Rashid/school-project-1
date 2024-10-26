5595604effb8888837737ade5c6ca1f96d099c7a
pepars-Rashid <peparsrashid@gmail.com>
2024-10-25 17:04:03 -0700

    a tiny bug

--- src/app/pep-testing/page.jsx
+++ src/app/pep-testing/page.jsx
@@ -1,11 +1,9 @@
 
 import Calander from '@/components/calander'
-import DashboardCon from '@/components/Dashboardcon'
+import DashboardCon from '@/components/DashboardCon'
 import StudentHome from '@/components/strudentDashboard/home/student-home'
 import StudentDashboard from '@/components/strudentDashboard/student-dashboard'
 import StudentPlanner from '@/components/strudentDashboard/student-planner/student-planner'
-
-
 
 export default function page() {
   return (
